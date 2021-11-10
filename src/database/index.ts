9be89c46ed869377e5adeaa3481c0c1f96a42a18
Lucas Lessa <lucaslessa14@hotmail.com>
2021-11-10 10:16:49 -0300

    fix(database): fixed database connection with environment variables

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,6 @@
 import { createConnection } from "typeorm";
 
-createConnection({
-    "type": "mysql",
-    "host": process.env.DB_HOST,
-    "port": 3306,
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "cli": {
-        "entitiesDir": "src/entity",
-        "migrationsDir": "src/database/migrations"
-    }
-}).then(connection => {
+createConnection().then(connection => {
     console.log("Connected to database");
 }).catch(error => {
     console.log("Error connecting to database: ", error);

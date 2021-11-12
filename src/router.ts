5e9692f9629c76650669417a3b73be5b2d12e0e5
Lucas Lessa <lucaslessa14@hotmail.com>
2021-11-12 08:57:51 -0300

    feat(champions): created delete method

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { CreateChampionController } from './controllers/champions/CreateChampionController';
 import { ListChampionsController } from './controllers/champions/ListChampionsController';
+import { DeleteChampionController } from './controllers/champions/DeleteChampionController';
 
 const router = express.Router();
 
 const createChampionController = new CreateChampionController();
 const listChampionsController = new ListChampionsController();
+const deleteChampionController = new DeleteChampionController();
 
 router.get('/champions', listChampionsController.handle);
 router.post('/champions', createChampionController.handle);
+router.delete('/champions/:id', deleteChampionController.handle);
 
 export { router };

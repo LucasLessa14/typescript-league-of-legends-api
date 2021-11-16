a75c9142643a48050874d0cfa09e6909a674a52b
Lucas Lessa <lucaslessa14@hotmail.com>
2021-11-16 18:00:28 -0300

    WIP(passive): service in progress

--- src/services/champions/UpdateChampionService.ts
+++ src/services/champions/UpdateChampionService.ts
@@ -1,35 +1,42 @@
 import { getCustomRepository } from "typeorm";
 import { ChampionsRepositories } from "../../repositories/ChampionsRepositories";
+import { ChampionPassiveRepositories } from "../../repositories/ChampionPassiveRepositories";
 
+interface IChampionRequest {
+    id: string;
+    name?: string;
+    role?: string;
+    lane?: string;
+    passiveId?: string;
+}
 class UpdateChampionService {
-    public async execute(id: string, name?: string, role?: string, lane?: string) {
+    public async execute({id, name, role, lane, passiveId}: IChampionRequest) {
 
         const championRepository = getCustomRepository(ChampionsRepositories);
 
         const champion = await championRepository.findOne(id);
 
-        if (!champion) {
-            throw new Error('Champion not found');
-        }
+        if (!champion) throw new Error('Champion not found');
 
-        if (name === undefined) {
-            name = champion.name;
-        }
+        if (name && champion.name != name) champion.name = name;
 
-        if (role === undefined) {
-            role = champion.role;
-        }
+        if (role && champion.role != role) champion.role = role;
 
-        if (lane === undefined) {
-            lane = champion.lane;
+        if (lane && champion.lane != lane) champion.lane = lane;
+
+        if (passiveId && champion.passive.id != passiveId) {
+            
+            const passiveRepository = getCustomRepository(ChampionPassiveRepositories);
+    
+            const passive = await passiveRepository.findOne(passiveId);
+    
+            if (!passive) throw new Error('Passive not found');
+
+            champion.passive = passive;
         }
 
         try {
-            const updateChampion = await championRepository.update(id, {
-                name,
-                role,
-                lane
-            });
+            const updateChampion = await championRepository.update(id, champion);
 
             return updateChampion;
 
